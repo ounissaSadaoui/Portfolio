4dfc14d2d64230a3f542ddc126d42ddb7198f8bf
ounissaSadaoui <120554715+ounissaSadaoui@users.noreply.github.com>
2024-01-10 10:05:48 +0100

    entame form.css

--- js/app.js
+++ js/app.js
@@ -4,13 +4,18 @@ document.addEventListener("DOMContentLoaded", e => {
     let nav_page = document.querySelector("nav");
     let icone_select = document.querySelectorAll("a i");
     let button = document.querySelector("button");
+    let icones = document.querySelectorAll("figure i");
 
     btn_change_mode.addEventListener("click", () => {
         parent_page.classList.toggle("darkmode");
         nav_page.classList.toggle("nav_darkmode");
         button.classList.toggle("darkmode");
+
+        for (let ones in icones) {
+            console.log(ones);
+            icones[ones].style.color = "rgb(53, 49, 49)";
+        }
         for (let row in icone_select) {
-            console.log(row);
             icone_select[row].style.color = "#fff";
         }
     });
